520a5b7afdd2f21d37aa9081c49e04b6edce628b
aliounendiaye221 <alioune.ndiaye29@unchk.edu.sn>
2026-05-08 12:22:02 +0000

    feat: add 5th demo video and fix mobile responsiveness issues

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             });
         }, {
-            threshold: 0.15,
-            rootMargin: '0px 0px -50px 0px'
+            threshold: 0.05,
+            rootMargin: '0px 0px -20px 0px'
         });
         revealElements.forEach(el => observer.observe(el));
     } else {
